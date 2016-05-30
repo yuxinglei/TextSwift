7f84b542087f6e603b58dd9c215efc90b0a79d7f
于兴雷 <yuxinglei@yuxingleideMacBook-Pro.local>
2016-05-30 22:11:53 +0800

    测试

--- TextSwift/TextSwift/ViewController.swift
+++ TextSwift/TextSwift/ViewController.swift
@@ -59,12 +59,12 @@ class ViewController: UIViewController , TextViewDeiegete {
         self.title = "text1";
         
         
-        print("The number is divisible by \(division[9]) times")
-        print("The number is divisible by \(division[2]) times")
-        print("The number is divisible by \(division[3]) times")
-        print("The number is divisible by \(division[5]) times")
-        print("The number is divisible by \(division[7]) times")
-        
+//        print("The number is divisible by \(division[9]) times")
+//        print("The number is divisible by \(division[2]) times")
+//        print("The number is divisible by \(division[3]) times")
+//        print("The number is divisible by \(division[5]) times")
+//        print("The number is divisible by \(division[7]) times")
+//        
 
         //枚举
        // let studMarks = Student.Mark(98,97,95);
